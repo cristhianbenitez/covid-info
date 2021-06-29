9a1044e54f00960029449fef2553aa2afc1d5fe2
Cristhian Benitez <cristhiantilleria@gmail.com>
2021-06-28 20:40:16 -0400

    minimal changes

--- src/api/data.js
+++
@@ -1,9 +0,0 @@
-const data = fetch('https://disease.sh/v3/covid-19/all')
-.then(res => res.json())
-.then(data => console.log((data)) )
-.catch(error => console.log('error'))
-
-export default data
-
-
-
